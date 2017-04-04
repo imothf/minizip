15df83f7304342525256b48bc9413dbff20224cc
Dmitry Dovgoshliubnyi <darthdimon@googlemail.com>
2017-04-04 13:24:52 +0100

    Renamde source folder

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "CMinizip",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/CZlib", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/imothf/CZlib", majorVersion: 0, minor: 4)
     ]
 )
 
